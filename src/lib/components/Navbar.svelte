6da8e4d907ba9ec9e0ecf6383bf6afb072c98b24
Corrado Cionini <corrado.cionini@protonmail.com>
2024-06-08 11:24:33 +0200

    feat: navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import logo from '$lib/assets/pocket_charge_logo.png';
+  import Button from './ui/button/button.svelte';
+</script>
+
+<nav class="bg-zinc-300 flex items-center justify-between px-8 py-4">
+  <div class="flex items-center">
+    <img class="w-16 h-16" src={logo} alt="pocketcharge logo" />
+    <a href="/" class="text-3xl text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-zinc-500">PocketCharge</a>
+  </div>
+  <div class="flex items-center space-x-2">
+    <Button href="/login">Login</Button>
+    <Button href="/signup">Sign up</Button>
+  </div>
+</nav>
