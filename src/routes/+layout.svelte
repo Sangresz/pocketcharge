23758fad064370d69de64ee247d66e03dd3381d5
Corrado Cionini <corrado.cionini@protonmail.com>
2024-06-08 13:31:43 +0200

    supabase init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,29 @@
-<script lang="ts">
-  import "../app.css"
+<script>
+  import '../app.css';
+	import { goto, invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	const { data, children } = $props();
+	let { session, supabase } = $derived(data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (!newSession) {
+				/**
+				 * Queue this as a task so the navigation won't prevent the
+				 * triggering function from completing
+				 */
+				setTimeout(() => {
+					goto('/', { invalidateAll: true });
+				});
+			}
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
-<slot />
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
   import Navbar from "$lib/components/Navbar.svelte";
+
+  const { data } = $props();
+
+  let countries = data.countries;
 </script>
 
 <Navbar />
+
+{#each countries as country}
+  <div>{country.name}</div>
+{/each}
