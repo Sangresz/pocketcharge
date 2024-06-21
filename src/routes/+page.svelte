34a95abbdbf69843a0e33fdaed73e29d55a2dd97
Corrado Cionini <corrado.cionini@protonmail.com>
2024-06-21 16:56:56 +0200

    feat: charges table with RLS policy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	const { data } = $props();
 
-	let countries = data.countries;
+	let charges = data.charges;
 </script>
 
-{#each countries as country}
-	<div>{country.name}</div>
-{/each}
+<div class="flex flex-col space-y-4">
+	{#each charges as charge}
+		<div class={`${charge.is_expense?"bg-red-300":"bg-green-300"} px-4 py-2 text-xl rounded-md`}>
+			<div class="flex space-x-2">
+				<div>
+					{charge.title}
+				</div>
+				<div>
+					{charge.amount}
+				</div>
+			</div>
+		</div>
+	{/each}
+</div>
