d5b9f2b00777664646a6c2a073c2e1c4979ab520
Corrado Cionini <corrado.cionini@protonmail.com>
2024-06-23 12:12:56 +0200

    db: create wallets table

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals: { supabase } }) => {
-	const { data: charges } = await supabase.from('charges').select('title,amount,is_expense');
+	const { data: charges } = await supabase.from('charges').select('name,amount,is_expense');
 
 	return { charges: charges ?? [] };
 };
