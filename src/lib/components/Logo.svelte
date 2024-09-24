6d01b350e1642406429f9d95aab997bfda362c7a
Corrado Cionini <corrado.cionini@protonmail.com>
2024-09-24 10:54:16 +0200

    feat: user logged in now cant go to "/"

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -2,10 +2,10 @@
 	import logo from '$lib/assets/logo.svg';
 </script>
 
-<a href="/" class="flex items-center space-x-2">
+<div class="flex items-center space-x-2">
 	<img class="w-8 h-8 md:w-16 md:h-16" src={logo} alt="pocketcharge logo" />
 	<div
 		class="text-lg lg:text-3xl text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-zinc-500"
 		>PocketCharge</div
 	>
-</a>
+</div>
