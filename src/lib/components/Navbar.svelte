43fb8f41fd89cfee029b3d4a9216beacd22e05c3
Corrado Cionini <corrado.cionini@protonmail.com>
2025-06-08 17:10:54 +0200

    supabase working without auth

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
-	import type RecordModel from 'pocketbase';
 	import Logo from '$lib/components/Logo.svelte';
 	import Button from './ui/button/button.svelte';
 
-	const { user } = $props<{user: RecordModel | undefined}>();
+	const { user } = $props();
 </script>
 
 <nav class="flex items-center justify-between py-4">
-	{#if !user}
-		<a href="/"><Logo /></a>
-		<div class="flex items-center space-x-4">
-			<Button href="/auth/signup">Sign up</Button>
-			<Button href="/auth/login">Login</Button>
-		</div>
-		{:else}
+	{#if user}
 		<a href="/app"><Logo /></a>
 		<div class="flex items-center space-x-4">
 			<span>{user.email}</span>
 			<Button href="/auth/logout">Logout</Button>
+		</div>
+	{:else}
+		<a href="/"><Logo /></a>
+		<div class="flex items-center space-x-4">
+			<Button href="/auth/signup">Sign up</Button>
+			<Button href="/auth/login">Login</Button>
 		</div>
 	{/if}
 </nav>
---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_KEY } from '$env/static/private';
+
+const supabaseUrl = 'https://sdmpkurjvvvclzhrnekb.supabase.co';
+
+export const supabase = createClient(supabaseUrl, SUPABASE_KEY);
