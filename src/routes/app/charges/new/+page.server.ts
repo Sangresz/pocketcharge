75138d2e5a8dc7a9961856ab8edc22abcf3d966b
Corrado Cionini <corrado.cionini@protonmail.com>
2025-06-23 17:54:58 +0200

    feat: new basic charges

--- src/routes/app/charges/new/+page.server.ts
+++ src/routes/app/charges/new/+page.server.ts
@@ -1,7 +1,24 @@
-import type { Actions } from "@sveltejs/kit";
+import { fail, redirect, type Actions } from "@sveltejs/kit";
 
 export const actions: Actions = {
-    default: async ({ locals: { user, supabase } }) => {
+    default: async ({ request, locals: { user, supabase } }) => {
+        const form_data = await request.formData();
 
+        const name = form_data.get('name') as string;
+        const amount = form_data.get('amount') as string;
+        const is_expense = form_data.get('is_expense') as string;
+
+        const { error } = await supabase.from('charges').insert({
+            name,
+            amount: parseInt(amount),
+            is_expense: is_expense === 'on',
+            user_id: user?.id
+        })
+
+        if (error) {
+            return fail(500, { error: 'Failed to update group' });
+        }
+
+        redirect(303, '/app');
     }
 }
