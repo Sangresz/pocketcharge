f74e213b615bbb361209f1829be49204eb1e252e
Corrado Cionini <corrado.cionini@protonmail.com>
2025-06-23 11:56:48 +0200

    feat: add people to group

--- src/routes/app/groups/[id]/+page.server.ts
+++ src/routes/app/groups/[id]/+page.server.ts
@@ -1,12 +1,12 @@
 import type { PageServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
+import { redirect, type Actions } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params, locals: { supabase, user } }) => {
     let groupId = params.id;
 
     const { data: group, error } = await supabase
         .from('groups')
-        .select('name, member_groups(name)')
+        .select('id, name, member_groups(name)')
         .eq('id', groupId)
         .limit(1);
 
@@ -22,4 +22,27 @@ export const load: PageServerLoad = async ({ params, locals: { supabase, user } 
     return {
         group: group[0]
     };
+};
+
+export const actions: Actions = {
+    addPeople: async ({ request, locals: { supabase } }) => {
+        const formData = await request.formData();
+        const names = formData.getAll('name') as string[];
+        const group_id = new URL(request.url).searchParams.get('group_id');
+
+        names.forEach(async (name) => {
+            const { error } = await supabase
+                .from('member_groups')
+                .insert({
+                    name,
+                    group_id
+                })
+
+            if (error) {
+                console.error('Error inserting profile:', error);
+            }
+        })
+
+        redirect(303, '/app/groups/' + group_id);
+    }
 };
