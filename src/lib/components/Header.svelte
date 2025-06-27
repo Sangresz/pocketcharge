c38b3f14a47bc60548f13f7f27843d8dcba4816c
Corrado Cionini <corrado.cionini@protonmail.com>
2025-06-27 09:58:23 +0200

    fix: navbar mobile

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,7 +4,7 @@
 	const { user } = $props();
 </script>
 
-<nav class="flex items-center justify-between py-4">
+<nav class="flex w-full items-center justify-between py-4">
 	{#if user}
 		<a href="/app"><Logo /></a>
 		<div class="flex items-center space-x-4">
